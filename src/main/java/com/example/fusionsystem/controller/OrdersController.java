89b2efd4b9693a7c51ec44a8094a80f456b534ff
tcydy <510167782@qq.com>
2026-06-12 23:27:20 +0800

    create

--- src/main/java/com/example/fusionsystem/controller/OrdersController.java
+++ src/main/java/com/example/fusionsystem/controller/OrdersController.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.fusionsystem.common.Result;
+import com.example.fusionsystem.enity.Account;
 import com.example.fusionsystem.enity.Address;
 import com.example.fusionsystem.enity.Goods;
 import com.example.fusionsystem.enity.Orders;
@@ -79,8 +80,42 @@ public class OrdersController {
     public Result findAll(){return Result.success(ordersService.list());}
 
     @GetMapping("/{id}")
-    public Result findOne(@PathVariable Integer id){return Result.success(ordersService.getById(id));}
+    public Result findOne(@PathVariable Integer id) {
+        return Result.success(ordersService.getById(id));
+    }
 
+    
+    @GetMapping("/front/page")
+    public Result findFrontPage(@RequestParam Integer pageNum,
+                                @RequestParam Integer pageSize, 
+                                @RequestParam String status,
+                                @RequestParam String flag,
+                                @RequestParam(defaultValue = "") String keyword){
+        
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByDesc(Orders::getId);
+
+        //状态转换
+        if (!StrUtil.equals(status, "全部")) {
+            queryWrapper.eq(Orders::getStatus, status);
+        }
+
+        Account account = TokenUtils.getCurrentUser();
+        
+        if (StrUtil.equals(flag, "我卖出的")) {
+            queryWrapper.eq(Orders::getFromId, getId());
+        }
+
+        if (StrUtil.equals(flag, "我买到的")) {
+            queryWrapper.eq(Orders::getToId, getId());
+        }
+
+        if (StrUtil.isNotBlank(keyword)) {
+            queryWrapper.like(Orders::getNo, keyword);
+        }
+        return Result.success(ordersService.page(new Page<>(pageNum, pageSize), queryWrapper));
+    }
+    
     @GetMapping("/page")
     public  Result findPage(@RequestParam Integer pageNum,
                             @RequestParam Integer pageSize,
--- vue-project/src/views/front/Orders.vue
+++ vue-project/src/views/front/Orders.vue
@@ -1,8 +1,49 @@
 <script setup>
-import { ref } from 'vue'
-import { Shop } from '@element-plus/icons-vue'
+//表格数据
+import {reactive, ref,shallowRef,onMounted} from "vue";
+import request from "@/utils/request.js";
+import {ElMessage} from "element-plus";
+import {Shop,Search} from "@element-plus/icons-vue";
 
-const sideBar = ref('我买到的')
+
+
+const sideBar = ref('我卖出的')
+const status = ref('全部')
+
+//表格数据
+const tableData=ref([])
+const total=ref(0)
+const pageNum=ref(1)
+const pageSize=ref(10)
+
+//搜索条件
+const searchForm=reactive({
+  keyword:'',
+})
+
+//加载数据
+const load=()=>{
+  request.get("/orders/front/page",{
+    params:{
+      pageNum:pageNum.value,
+      pageSize:pageSize.value,
+      keyword:searchForm.keyword,
+      status:status.value,
+      flag:sideBar.value,
+    }
+  }).then(res => {
+    console.log('res:', res)  // 调试用
+    if (res.code !== '200') {
+        ElMessage.error(res.message)
+        return
+    }
+    if(res.data){
+        tableData.value = res.data.records
+        total.value = res.data.total
+        //console.log('加载订单数据:', tableData)  // 调试用
+    }
+  })
+}
 
 </script>
 
@@ -17,26 +58,29 @@ const sideBar = ref('我买到的')
                         <h3>我的交易</h3>
                     </div>
                     
-                    <div class="button-buy-sale":class="{'active': sideBar==='我买到的'}" @click="sideBar='我买到的'">
-                        我买到的
-                    </div>
-
                     <div class="button-buy-sale":class="{'active': sideBar==='我卖出的'}" @click="sideBar='我卖出的'">
                         我卖出的
                     </div>
 
-                    <div>
-                        
+                    <div class="button-buy-sale":class="{'active': sideBar==='我买到的'}" @click="sideBar='我买到的'">
+                        我买到的
                     </div>
-
                 </el-card>
             </div>
 
             <div style="flex:1;display: flex;flex-direction: column;gap:10px;">
-                <el-card style="border-radius: 10px">
-
+                <el-card class="search-card" >
+                    <el-input class="search-input" v-model="searchForm.keyword"placeholder="请输入订单号" clear="filter-input":prefix-icon="Search" clearable/>
+                    <el-button class="ml-10" plain type="primary"@click="load">搜索</el-button>
                 </el-card>
+
                 <el-card style="border-radius: 10px">
+                    <el-tabs v-model="status" @tab-change="load">
+                        <el-tab-pane label="全部" name="全部"></el-tab-pane>
+                        <el-tab-pane label="待支付" name="待支付"></el-tab-pane>
+                        <el-tab-pane label="待发货" name="待发货"></el-tab-pane>
+                        <el-tab-pane label="待收货" name="待收货"></el-tab-pane>
+                    </el-tabs>
 
                 </el-card>
                 <div>
@@ -77,4 +121,21 @@ const sideBar = ref('我买到的')
         font-size: 22px;
     }
 }
+.search-input{
+    width:300px;
+}
+.search-card{
+    border-radius: 10px;
+    padding:0px;
+    .search-input{
+        padding:0px;
+        margin-right: 10px;
+        width: 90%;
+        height:40px;
+    }
+    .search-input :deep(.el-input__wrapper) {
+    border-radius: 10px !important;
+    }
+}
+
 </style>
